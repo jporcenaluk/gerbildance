89e1fd2e739dff0e41933127cec894c176f6e3c3
Jared Porcenaluk <jporcenaluk@live.com>
2016-02-27 14:38:26 -0500

    Changed where files live (in wwwroot). Added js and css files (empty). Started work on connecting to service.

--- src/GerbilDance/Startup.cs
+++ src/GerbilDance/Startup.cs
@@ -20,12 +20,9 @@ namespace GerbilDance
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app)
         {
-            app.UseIISPlatformHandler();
-
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
+            //app.UseIISPlatformHandler();
+            app.UseDefaultFiles();
+            app.UseStaticFiles(); ;
         }
 
         // Entry point for the application.
---
+++ src/GerbilDance/wwwroot/js/connection.js
@@ -0,0 +1,5 @@
+﻿/// <reference path="jquery-2.2.1.min.js" />
+
+$(document).ready(function () {
+    $.post("http://dribbledance.apiblueprint.org/register", success(alert("hello")));
+});
