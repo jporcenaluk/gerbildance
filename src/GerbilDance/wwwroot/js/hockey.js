aab280ebf20a3082db7d474dc6414333d350c88f
Jared Porcenaluk <jporcenaluk@live.com>
2016-02-27 20:26:01 -0500

    Working on making sure there's no duplicates in the gaem.

--- src/GerbilDance/wwwroot/js/hockey.js
+++ src/GerbilDance/wwwroot/js/hockey.js
@@ -1,27 +1,50 @@
 ﻿$(function () {
-
+    //Get all the touchable items
     var items = $(".touch-circle");
-    items.click(function (item) {
-    
-        //If it has an active class, it is highlighted and it can move on to a new circle
-        if ($(this).hasClass("active")) {
-            SetNewTouchCircle($(this));
-        }
-    });
 
-    function SetNewTouchCircle(OldCircle) {
+    items.each(function (item, i) {
 
-        var itemIndex = Math.floor(Math.random() * items.length);
+        $(this).click(function () {
 
-        //If it already has an active class, try again
-        if ($(items[itemIndex]).hasClass("active")) {
-            SetNewTouchCircle(OldCircle);
-        }
-        else {
-            $(items[itemIndex]).addClass("active");
-            //Remove old highlight
-            $(OldCircle).removeClass("active");
-        }
+            items.removeClass('active');
 
-    }
+            // pick a new one
+            var index = i;
+
+            do { 
+                index = Math.floor(Math.random() * items.length);
+            }
+            while (index == i);
+         
+            $(items[index]).addClass("active");
+        })
+    })
+
+    //items.removeClass('active')
+
+
+    //var items = $(".touch-circle");
+    //items.click(function (item) {
+
+    //    //If it has an active class, it is highlighted and it can move on to a new circle
+    //    if ($(this).hasClass("active")) {
+    //        SetNewTouchCircle($(this));
+    //    }
+    //});
+
+    //function SetNewTouchCircle(OldCircle) {
+
+    //    var itemIndex = Math.floor(Math.random() * items.length);
+
+    //    //If it already has an active class, try again
+    //    if ($(items[itemIndex]).hasClass("active")) {
+    //        SetNewTouchCircle(OldCircle);
+    //    }
+    //    else {
+    //        $(items[itemIndex]).addClass("active");
+    //        //Remove old highlight
+    //        $(OldCircle).removeClass("active");
+    //    }
+
+    //}
 });
