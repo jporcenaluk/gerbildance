423fbcf8036f998e7f9e7c9c2bb4ee0baceede29
Daniel Walker <daniel@fattmerchant.com>
2016-02-27 20:52:47 -0500

    convert 4 spaces to 2

--- src/GerbilDance/wwwroot/js/connection.js
+++ src/GerbilDance/wwwroot/js/connection.js
@@ -2,52 +2,52 @@
 
 $(document).ready(function () {
 
-    const games = {
-        basketball: {
-            teams: { 1: "blue", 2: "silver", 3: "white", 4: "black" },
+  const games = {
+    basketball: {
+      teams: { 1: "blue", 2: "silver", 3: "white", 4: "black" },
 
-        },
-        hockey: {
-            teams: { 1: "blue", 2: "silver", 3: "white", 4: "black" }
-        }
+    },
+    hockey: {
+      teams: { 1: "blue", 2: "silver", 3: "white", 4: "black" }
     }
+  }
 
-    //Initialize game
-    GetGame();
+  //Initialize game
+  GetGame();
 
-    function GetGame() {
+  function GetGame() {
 
-        ///Always get the leaderboard
-        ///If no leaderboard, play game
-        $.ajax({
-            type: "POST",
-            url: "http://private-143c2-dribbledance.apiary-mock.com/register",
-            dataType: "json",
-            success: function (data) {
-                InitializeGame(data.game_type);
-                InitializeTeam(data);
-                $("#loading").hide();
-            },
-            error: function (data) {
-                alert("failed: " + data);
-            }
-        });
-    }
-
-    function InitializeGame(gameId) {
-        $("#game").show();
-        $("#" + gameId).show();
-    }
-
-    function InitializeTeam(data) {
-        var teamName = data.team;
-        var gameType = games[data.game_type];
-        var teamName = gameType.teams[teamName];
-        $("body").addClass("team-" + teamName);
-    }
-
-    $(".see-leaderboard").click(function () {
-        $("#game").hide();
-        $("#leaderboard").show();
+    ///Always get the leaderboard
+    ///If no leaderboard, play game
+    $.ajax({
+      type: "POST",
+      url: "http://private-143c2-dribbledance.apiary-mock.com/register",
+      dataType: "json",
+      success: function (data) {
+        InitializeGame(data.game_type);
+        InitializeTeam(data);
+        $("#loading").hide();
+      },
+      error: function (data) {
+        alert("failed: " + data);
+      }
     });
+  }
+
+  function InitializeGame(gameId) {
+    $("#game").show();
+    $("#" + gameId).show();
+  }
+
+  function InitializeTeam(data) {
+    var teamName = data.team;
+    var gameType = games[data.game_type];
+    var teamName = gameType.teams[teamName];
+    $("body").addClass("team-" + teamName);
+  }
+
+  $(".see-leaderboard").click(function () {
+    $("#game").hide();
+    $("#leaderboard").show();
+  });
 });
--- src/GerbilDance/wwwroot/js/hockey.js
+++ src/GerbilDance/wwwroot/js/hockey.js
@@ -1,50 +1,50 @@
 ﻿$(function () {
-    //Get all the touchable items
-    var items = $(".touch-circle");
+  //Get all the touchable items
+  var items = $(".touch-circle");
 
-    items.each(function (item, i) {
+  items.each(function (item, i) {
 
-        $(this).click(function () {
+    $(this).click(function () {
 
-            items.removeClass('active');
+      items.removeClass('active');
 
-            // pick a new one
-            var index = i;
+      // pick a new one
+      var index = i;
 
-            do { 
-                index = Math.floor(Math.random() * items.length);
-            }
-            while (index == i);
-         
-            $(items[index]).addClass("active");
-        })
+      do { 
+        index = Math.floor(Math.random() * items.length);
+      }
+      while (index == i);
+     
+      $(items[index]).addClass("active");
     })
+  })
 
-    //items.removeClass('active')
+  //items.removeClass('active')
 
 
-    //var items = $(".touch-circle");
-    //items.click(function (item) {
+  //var items = $(".touch-circle");
+  //items.click(function (item) {
 
-    //    //If it has an active class, it is highlighted and it can move on to a new circle
-    //    if ($(this).hasClass("active")) {
-    //        SetNewTouchCircle($(this));
-    //    }
-    //});
+  //  //If it has an active class, it is highlighted and it can move on to a new circle
+  //  if ($(this).hasClass("active")) {
+  //    SetNewTouchCircle($(this));
+  //  }
+  //});
 
-    //function SetNewTouchCircle(OldCircle) {
+  //function SetNewTouchCircle(OldCircle) {
 
-    //    var itemIndex = Math.floor(Math.random() * items.length);
+  //  var itemIndex = Math.floor(Math.random() * items.length);
 
-    //    //If it already has an active class, try again
-    //    if ($(items[itemIndex]).hasClass("active")) {
-    //        SetNewTouchCircle(OldCircle);
-    //    }
-    //    else {
-    //        $(items[itemIndex]).addClass("active");
-    //        //Remove old highlight
-    //        $(OldCircle).removeClass("active");
-    //    }
+  //  //If it already has an active class, try again
+  //  if ($(items[itemIndex]).hasClass("active")) {
+  //    SetNewTouchCircle(OldCircle);
+  //  }
+  //  else {
+  //    $(items[itemIndex]).addClass("active");
+  //    //Remove old highlight
+  //    $(OldCircle).removeClass("active");
+  //  }
 
-    //}
+  //}
 });
