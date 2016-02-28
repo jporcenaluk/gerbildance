8230e9a55fd6dc6fc8f3e4e0ddf20ea221bf9b0c
Jared Porcenaluk <jporcenaluk@live.com>
2016-02-28 09:42:28 -0500

    Moved basketball game code from hockey.js to basketball.js

--- src/GerbilDance/wwwroot/js/basketball.js
+++ src/GerbilDance/wwwroot/js/basketball.js
@@ -1,1 +1,23 @@
-﻿
+﻿$(function () {
+    //Get all the touchable items
+    var items = $(".touch-circle");
+
+    items.each(function (item, i) {
+
+        $(this).click(function () {
+
+            items.removeClass('active');
+
+            // pick a new one
+            var index = i;
+
+            do {
+                index = Math.floor(Math.random() * items.length);
+            }
+            while (index == i);
+
+            $(items[index]).addClass("active");
+        })
+    })
+
+});
